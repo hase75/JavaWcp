c749c9f3e1280c1ac9ca93e58daf09119c3fa976
Keigo <122374696+hase75@users.noreply.github.com>
2023-08-10 19:12:50 +0900

    Chapter6 完了

---
+++ src/Chapter05.java
@@ -0,0 +1,16 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+			String str = "Keigo";
+			System.out.println(str);
+			
+			str = "プログラミング";
+			System.out.println(str);
+			
+			final int luckyNumber = 7;
+			System.out.println(luckyNumber);
+	}
+
+}
