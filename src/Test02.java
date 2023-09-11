10691e3226e3281a3cac62af180f32fe9d4de5a0
Keigo <122374696+hase75@users.noreply.github.com>
2023-09-11 20:23:14 +0900

    テスト予習

--- src/Test02.java
+++ src/Test02.java
@@ -27,16 +27,16 @@ public class Test02 {
         }
         
 //      2問目a　1〜10までに偶数が何個あるか、また偶数の合計は幾つになるか表示させる
-        int count = 0;
-        int sum = 0;
-        
-        for (int i = 2; i <= 10; i += 2) {
-        	count ++;
-        	sum += i;
-        }
-        
-        System.out.println("偶数の個数は" + count);
-        System.out.println("偶数の合計は" + sum);
+       int count = 0;
+       int sum = 0;
+       
+       for (int i = 2; i <= 10; i += 2) {
+    	   count ++;
+    	   sum += i;
+       }
+       
+       System.out.println("偶数の個数は" + count);
+       System.out.println("偶数の合計は" + sum);
         
 //      2問目b　1〜10までに奇数が何個あるか、また奇数の合計は幾つになるか表示させる
         int count2 = 0;
@@ -53,7 +53,7 @@ public class Test02 {
 //      3問目a　1〜10までの整数をカンマ区切りで横並びで表示させる（10にはカンマをつけない）
         for (int i = 1; i <= 10; i ++) {
         	if (i < 10) {
-        		System.out.print(i + ",");
+        		System.out.print(i + ", ");
         	} else {
         		System.out.print(i);
         	}
@@ -62,7 +62,7 @@ public class Test02 {
 //      3問目b　1〜10までの偶数をカンマ区切りで横並びで表示させる（最後はカンマをつけない）
        for (int i = 2; i <= 10; i += 2) {
     	   if (i < 10) {
-    		   System.out.print(i + ",");
+    		   System.out.print(i + ", ");
     	   } else {
     		   System.out.print(i);
     	   }
@@ -71,40 +71,40 @@ public class Test02 {
 //      3問目c　1〜10までの奇数をカンマ区切りで横並びで表示させる（最後はカンマをつけない）
         for (int i = 1; i <= 10; i += 2) {
         	if (i < 9) {
-        		System.out.print(i + ",");
+        		System.out.print(i + ", ");
         	} else {
         		System.out.print(i);
         	}
         }
 
-//      4問目　要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
-        int[] array = new int[20];
-        
-        for (int i = 0; i < 20; i ++) {
-        	array[i] = i * 5;
-        }
-        
-        System.out.print("\n奇数:");
-        
-        for (int i = 0; i <= 10; i ++) {
-        	if (array[i] % 2 != 0) {
-        		System.out.print(array[i]);
-        		if (i < 9) {
-        			System.out.print(",");
-        		}
-        	}
-        }
-        
-        System.out.print("\n偶数:");
-        
-        for (int i = 11; i <= 19; i ++) {
-        	if (array[i] % 2 == 0) {
-        		System.out.print(array[i]);
-        		if (i < 18) {
-        			System.out.print(",");
-        		}
-        	}
-        }
-
+//      4問目　要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。
+//            [0]~[10]の値の奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
+       int[] array = new int[20];
+       
+       for(int i = 0; i < 20; i ++) {
+    	   array[i] = i * 5;
+       }
+       
+       System.out.print("\n奇数:");
+       
+       for (int i = 0; i <= 10; i ++) {
+    	   if (array[i] % 2 != 0) {
+    		   System.out.print(array[i]);
+    		   if (i < 9) {
+    			   System.out.print(", ");
+    		   }
+    	   }
+       }
+       
+       System.out.print("\n偶数:");
+       
+       for (int i = 11; i <= 19; i ++) {
+    	   if (array[i] % 2 == 0) {
+    		   System.out.print(array[i]);
+    		   if (i < 18) {
+    			   System.out.print(", ");
+    		   }
+    	   }
+       }
 	}
 }
